4324d98cfbd2072a3e34d9091ebe236b7d394077
hiraniziyan <141653186+hiraniziyan@users.noreply.github.com>
2025-12-24 00:22:16 -0500

    Add files via upload

---
+++ projects/WebDev/website/src/app/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Hero from '@/components/Hero';
+import LogoCollection from '@/components/LogoCollection';
+import Features from '@/components/Features';
+import { Divider } from '@mui/material';
+import Testimonials from '@/components/Testimonials';
+import Pricing from '@/components/Pricing';
+import Footer from '@/components/Footer';
+import FAQ from '@/components/FAQ';
+import Highlights from '@/components/Highlights';
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Hero />
+        <div>
+          <LogoCollection />
+
+          <Features />
+
+          <Divider />
+
+          <Testimonials />
+
+          <Divider />
+
+          <Highlights />
+
+          <Divider />
+
+          <Pricing />
+
+          <Divider />
+
+          <FAQ />
+
+          <Divider />
+          
+          <Footer />
+        </div>
+      </Box>
+    </Container>
+  );
+}
